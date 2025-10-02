b41473d6880fe2af8be32ac60034b8f5487a6317
Horea Oros <horea.oros@gmail.com>
2025-10-02 12:47:35 +0300

    Add random demo

--- 01 - 2 oct/01 - 2 oct/Program.cs
+++ 01 - 2 oct/01 - 2 oct/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Random rnd = new Random();
+
+int number = rnd.Next(1, 101);
+
+// 56, 58, 67, 42
+
+Console.WriteLine(number);
