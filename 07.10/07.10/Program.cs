7d3e6c4b7fca218e5b0914d37fbbf1682505f6b9
Horea Oros <horea.oros@gmail.com>
2025-10-07 09:48:00 +0300

    add conversion project

---
+++ 07.10/07.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+int n;
+Console.WriteLine(int.MinValue);
+Console.WriteLine(int.MaxValue);
+
+
+uint un;
+un = uint.MaxValue;
+Console.WriteLine(un);
+
+n = 42;
+Console.WriteLine(ConvertToBase2(n));
+
+string ConvertToBase2(int n)
+{
+    Stack<int> digits = new Stack<int>();
+    while (n > 0)
+    {
+        int r = n % 2;
+        digits.Push(r);
+        n /= 2; // n = n / 2;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (digits.Count > 0)
+    {
+        sb.Append(digits.Pop());
+    }
+
+    return sb.ToString();
+}
