a12baf7f53c6ac90e4048d0aaa4b1780d605e512
Horea Oros <horea.oros@gmail.com>
2025-10-09 09:51:27 +0300

    add first project

---
+++ 0910/0910/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text;
+
+int n = 125894;
+
+Console.WriteLine(ConvertToBase2(n));
+
+string ConvertToBase2(int n)
+{
+    Stack<int> st = new Stack<int>();
+    while(n  > 0)
+    {
+        st.Push(n % 2);
+        n = n / 2;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while(st.Count > 0)
+    {
+        sb.Append(st.Pop().ToString());
+    }
+
+    return sb.ToString();
+}
