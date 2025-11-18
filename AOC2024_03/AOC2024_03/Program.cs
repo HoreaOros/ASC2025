60f34ff1ec9a139987a8bec2731f7db6e670d876
Horea Oros <horea.oros@gmail.com>
2025-11-18 09:50:09 +0200

    add AOC 2024/3

---
+++ AOC2024_03/AOC2024_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+FileStream fs = File.Open("input.txt", FileMode.Open);
+StreamReader sr = new StreamReader(fs);
+string text = sr.ReadToEnd();
+sr.Close();
+fs.Close();
+
+//mul(666,581)
+Regex r = new Regex(@"mul\(\d+,\d+\)");
+MatchCollection mc = r.Matches(text);
+long part1 = 0;
+foreach (Match m in mc)
+{
+    string s1 = m.Value
+        .Substring(3)
+        .Trim('(', ')');
+    string[] token = s1.Split(',');
+    long op1 = long.Parse(token[0]);
+    long op2 = long.Parse(token[1]);
+    part1 += op1 * op2;
+}
+Console.WriteLine(part1);
+
+
+Regex r2 = new Regex(@"mul\(\d+,\d+\)|do\(\)|don't\(\)");
+mc = r2.Matches(text);
+Console.WriteLine();
